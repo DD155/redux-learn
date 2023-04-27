8335a61d101f7bea1416f12970a0cca825999f81
DD155 <31744748+DD155@users.noreply.github.com>
2023-04-27 11:20:15 -0400

    end tutorial 6

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const CAKE_ORDERED = 'CAKE_ORDERED'
+
+/*
+actions are an object that have a type property
+
+action creators are functions that return actions
+*/
+const orderCake = () => {
+    return {
+        type: CAKE_ORDERED,
+        quantity: 1
+    }
+}
