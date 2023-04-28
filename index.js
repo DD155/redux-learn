99f5c064a2866dfbde83ffcf9455b8061037b421
DD155 <31744748+DD155@users.noreply.github.com>
2023-04-28 11:31:17 -0400

    finish actions, reducers, store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require('redux')
+const configureStore = redux.configureStore
+
 const CAKE_ORDERED = 'CAKE_ORDERED'
 
 /*
@@ -11,3 +14,52 @@ const orderCake = () => {
         quantity: 1
     }
 }
+
+
+// a state is an object 
+const initialState = {
+    numOfCakes: 10
+}
+
+/*
+the reducers are used to update a state
+reducer: function (previousState, action) => newState
+which will return a new state
+
+when returning the state with altered properties, make sure to use the spread operator
+to create a copy of the object
+*/
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            }
+        default: 
+            return state
+    }
+} 
+
+// create the redux store using the reducer as its argument
+const store = redux.legacy_createStore(reducer)
+console.log(`Initial state ${store.getState().numOfCakes}`)
+
+
+const unsubscribe = store.subscribe(() =>
+    console.log('update state ', store.getState()))
+
+store.dispatch(orderCake())
+unsubscribe()
+
+/*
+Redux pattern:
+1. Create the store
+2. Declare the initial state and reducer
+3. Define the action and action creator
+4. Subscribe to store
+5. Dispatch to perform the actions of the store
+6. Unsubscribe
+
+
+*/
