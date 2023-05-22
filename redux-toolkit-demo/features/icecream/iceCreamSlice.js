ed5ee6d187c81bf89080a84caea01a1a96e89cb0
DD155 <31744748+DD155@users.noreply.github.com>
2023-05-22 12:19:19 -0400

    learned extraReducers

--- redux-toolkit-demo/features/icecream/iceCreamSlice.js
+++ redux-toolkit-demo/features/icecream/iceCreamSlice.js
@@ -1,3 +1,5 @@
+const { cakeActions } = require('../cake/cakeSlice')
+
 const createSlice = require('@reduxjs/toolkit').createSlice
 
 const initialState = {
@@ -14,9 +16,20 @@ const iceCreamSlice = createSlice({
         restocked: (state, action) => {
             state.numOfIceCream += action.payload || 1
         }
+    },
+    // extraReducers: {
+    //     ['cake/ordered']: (state, action) => {
+    //         state.numOfIceCream--
+    //     }
+    // }
+    extraReducers: (builder) => {
+        builder.addCase(cakeActions.ordered, state => {
+            state.numOfIceCream--
+        })
     }
 })
 
 module.exports = iceCreamSlice.reducer
 module.exports.iceCreamActions = iceCreamSlice.actions 
 
+// extra reducers allows the slice to respond to another action and does not create a new action 
