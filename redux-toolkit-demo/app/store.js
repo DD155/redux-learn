c0aa02847cb3a56773eb6064a8d10b1113da0ecb
DD155 <31744748+DD155@users.noreply.github.com>
2023-05-22 12:07:57 -0400

    added logger middleware

--- redux-toolkit-demo/app/store.js
+++ redux-toolkit-demo/app/store.js
@@ -1,12 +1,17 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
+const reduxLogger = require('redux-logger')
+const logger = reduxLogger.createLogger()
+
 const cakeReducer = require('../features/cake/cakeSlice')
 const iceCreamReducer = require('../features/icecream/iceCreamSlice')
+const { getDefaultMiddleware } = require('@reduxjs/toolkit')
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         iceCream: iceCreamReducer,
     },
+    middleware: (getDefaultMiddleware) => (getDefaultMiddleware().concat(logger)),
 })
 
 module.exports = store
--- redux-toolkit-demo/index.js
+++ redux-toolkit-demo/index.js
@@ -3,9 +3,7 @@ const cakeActions = require('./features/cake/cakeSlice').cakeActions
 const iceCreamActions = require('./features/icecream/iceCreamSlice').iceCreamActions
 
 console.log('Inital state ', store.getState())
-const unsubscribe = store.subscribe(() => {
-    console.log('Updated state ', store.getState())
-})
+const unsubscribe = store.subscribe(() => {})
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.restocked(1))
