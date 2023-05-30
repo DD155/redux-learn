96f8aff1689305b9ed9556c21e404037a1e6612a
DD155 <31744748+DD155@users.noreply.github.com>
2023-05-30 12:44:35 -0400

    added providers

--- react-redux-demo/src/features/cake/CakeView.jsx
+++ react-redux-demo/src/features/cake/CakeView.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const CakeView = () => {
-  return (
+    const numOfCakes = useSelector(state => state.cake.numOfCakes)
+    
+    return (
     <div>
-        <h2> Number of Cakes </h2>
+        <h2> Number of Cakes: {numOfCakes}</h2>
         <button> Order Cake </button>
         <button> Restock Cake </button>
     </div>
-  )
+    )
 }
 
 export default CakeView
