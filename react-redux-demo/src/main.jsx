e0e699925a5a338369498acdd51ae1aafed94af3
DD155 <31744748+DD155@users.noreply.github.com>
2023-05-30 12:43:54 -0400

    added providers

--- react-redux-demo/src/main.jsx
+++ react-redux-demo/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {Prodider} from 'react-redux'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Prodider>
+        <App />
+    </Prodider> 
   </React.StrictMode>,
 )
