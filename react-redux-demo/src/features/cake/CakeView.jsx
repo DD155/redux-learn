c2c795a27d2ee1369f7d828788707c5b948b8cab
DD155 <31744748+DD155@users.noreply.github.com>
2023-05-30 12:23:41 -0400

    Revert "added providers"
    This reverts commit df0d575c1a4f3d4b3d1c561f10472776819a058e.

--- react-redux-demo/src/features/cake/CakeView.jsx
+++ react-redux-demo/src/features/cake/CakeView.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CakeView = () => {
   return (
     <div>
-        <h2> Number of Cakes: </h2>
+        <h2> Number of Cakes </h2>
         <button> Order Cake </button>
         <button> Restock Cake </button>
     </div>
